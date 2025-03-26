d89e3d58da5e203ac2b4db23c1b4037bb12f623e
Manendra Singh <singhmanendra349@gmail.com>
2025-03-26 10:36:02 +0530

    second commit

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/login', (req, res) => {
+    res.send('login UI')
+})
+
 app.listen(process.env.port, () => {
   console.log(`Example app listening on port ${process.env.port}`)
 })
